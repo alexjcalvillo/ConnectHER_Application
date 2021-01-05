e5769e628561b2756022dd21797efcde4459478a
Lara Komosa <larakomosa@gmail.com>
2021-01-05 15:41:14 -0600

    formatting registration form

--- src/components/MembershipOptions/MembershipOptions.js
+++ src/components/MembershipOptions/MembershipOptions.js
@@ -37,18 +37,47 @@ class RegistrationOptions extends Component {
         <Row>
           <Col className="registrationOptions" lg={4} md={4} sm={6} xs={6}>
             <Card>
-              <CardBody className="registrationOptions">
-                <h4>Plan 1</h4>
-                <Button
-                  color="info"
-                  outline
-                  size="sm"
-                  onClick={this.handleRegister}
-                  className="payGate"
-                >
-                  Register
-                </Button>{' '}
-              </CardBody>
+              <h3
+                style={{
+                  marginTop: '0px',
+                  marginBottom: 0,
+                  marginLeft: 10,
+                  fontSize: 25,
+                  fontFamily: 'Cabin',
+                }}
+              >
+                Plan 1
+              </h3>
+              <h5
+                style={{
+                  marginTop: 0,
+                  marginLeft: 10,
+                  fontSize: 13,
+                  fontFamily: 'Cabin',
+                }}
+              >
+                $10/Month
+              </h5>
+              <p
+                style={{
+                  marginTop: 0,
+                  marginLeft: 10,
+                  fontSize: 10,
+                  fontFamily: 'Lato:wght@300',
+                }}
+              >
+                includes access to membership database and receive 50% off
+                premium events
+              </p>
+              <Button
+                color="info"
+                outline
+                size="sm"
+                onClick={this.handleRegister}
+                className="payGate"
+              >
+                Register
+              </Button>{' '}
             </Card>
           </Col>
           <Col className="registrationOptions" lg={4} md={4} sm={6} xs={6}>
