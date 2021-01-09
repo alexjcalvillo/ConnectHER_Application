84eadbdcaa3548abe43b8caccd2d5b6a953126e3
Chase Simmons <csim6890@gmail.com>
2021-01-08 18:26:04 -0600

    front end messaging prep w/test data

--- src/components/FavoritesAndChat/ChatComponent.js
+++ src/components/FavoritesAndChat/ChatComponent.js
@@ -2,45 +2,54 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
+import ChatUser from './ChatUser';
+
 class ChatComponent extends Component {
   state = {
     view: undefined,
+    userId: undefined,
   };
 
-  toggleView = (view) => {
+  componentDidMount() {
+    this.props.dispatch({
+      type: 'FETCH_ALL_PROFILES',
+    });
+  }
+
+  toggleView = (data) => {
     this.setState({
-      view,
+      view: data.view,
+      userId: data.userId,
     });
   };
+
   render() {
-    let Content = (
-      <>
-        <div className={'tabListItem'}>
-          <div className="tabProfileImageContainer">
-            <img className="tabProfileImage" alt="img" />
-          </div>
-          <div className="tabNameContainer">
-            <p className="tabName">test</p>
-          </div>
-        </div>
-        <div className={'tabListItem'}>
-          <div className="tabProfileImageContainer">
-            <img className="tabProfileImage" alt="img" />
-          </div>
-          <div className="tabNameContainer">
-            <p className="tabName">test</p>
-          </div>
-        </div>
-        <div className={'tabListItem'}>
-          <div className="tabProfileImageContainer">
-            <img className="tabProfileImage" alt="img" />
-          </div>
-          <div className="tabNameContainer">
-            <p className="tabName">test</p>
-          </div>
-        </div>
-      </>
-    );
+    let memberArray = ['1', '2', '3'];
+    let Content;
+
+    if (this.state.view === undefined) {
+      Content = (
+        <>
+          {memberArray.map((member, index) => {
+            for (let i = 0; i < this.props.store.favorites.member.length; i++) {
+              if (member == this.props.store.memberListingsReducer[i].user_id) {
+                return (
+                  <ChatUser
+                    user={this.props.store.memberListingsReducer[i]}
+                    view={this.state.view}
+                    toggleView={(data) => {
+                      this.toggleView(data);
+                    }}
+                  />
+                );
+              }
+            }
+          })}
+        </>
+      );
+    } else {
+      Content = <div></div>;
+    }
 
     return (
       <>
@@ -51,7 +60,7 @@ class ChatComponent extends Component {
           <div
             className="popupReturn"
             onClick={() => {
-              this.toggleView(undefined);
+              this.toggleView({ view: undefined, userId: undefined });
             }}
           >
             <i className="fa fa-arrow-left popupReturnIcon" />
---
+++ src/components/FavoritesAndChat/ChatUser.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+
+class ChatUser extends Component {
+  state = {
+    view: undefined,
+  };
+
+  render() {
+    return (
+      <div
+        className={'tabListItem'}
+        onClick={() => {
+          this.props.toggleView({
+            view: 'chat',
+            userId: this.props.user.user_id,
+          });
+        }}
+      >
+        <div className="tabProfileImageContainer">
+          <img
+            className="tabProfileImage"
+            src={this.props.user.headshot}
+            alt="img"
+          />
+        </div>
+        <div className="tabNameContainer">
+          <p className="tabName">{this.props.user.display_name}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStoreToProps)(ChatUser);
