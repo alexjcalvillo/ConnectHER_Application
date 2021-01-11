c1f3791492b19a1e00828b8f20631c75f5ed4878
Chase Simmons <csim6890@gmail.com>
2021-01-11 11:15:09 -0600

    fix first chat instance

--- src/functions/doesChatInstanceExist.js
+++ src/functions/doesChatInstanceExist.js
@@ -1,19 +1,27 @@
 const doesChatInstanceExist = (data) => {
   let chatInstance;
-  for (let i = 0; i < data.list.length; i++) {
-    if (data.myId === data.list[i].user1 || data.myId === data.list[i].user2) {
+  console.log(data);
+  if (data.list.length !== 0) {
+    for (let i = 0; i < data.list.length; i++) {
       if (
-        data.userId === data.list[i].user1 ||
-        data.userId === data.list[i].user2
+        data.myId === data.list[i].user1 ||
+        data.myId === data.list[i].user2
       ) {
-        chatInstance = data.list[i].chatId;
-        i = data.list.length;
+        if (
+          data.userId === data.list[i].user1 ||
+          data.userId === data.list[i].user2
+        ) {
+          chatInstance = data.list[i].chatId;
+          i = data.list.length;
+        } else {
+          chatInstance = false;
+        }
       } else {
         chatInstance = false;
       }
-    } else {
-      chatInstance = false;
     }
+  } else {
+    chatInstance = false;
   }
   return chatInstance;
 };
