a120627040ccd56e7a6861a55956fc1aff1440e2
Lara Komosa <larakomosa@gmail.com>
2021-01-12 10:39:47 -0600

    database.sql and init.sql updated

--- database/data.sql
+++ database/data.sql
@@ -77,11 +77,5 @@ VALUES (1, 357), (1, 388), (1, 389), (1, 300), (1, 381),
 INSERT INTO "demographic" (age, ethnicity, gender, sexual_orientation, ability, income, education, user_id)
 VALUES ('18-24', 'Multiracial', 'Female / Female-Identifying', 'Straight / Heterosexual', 'I do not have a disability', '40000-79999', 'Some College', 1);
 
-CREATE TABLE "Member_Level" (
-    "id" SERIAL PRIMARY KEY,
-    "user_id" int REFERENCES "users",
-    "access_level" int 
-);
-
 INSERT INTO "member" (user_id, member_level) 
 VALUES (1, 2), (2,3), (3,3),(4,1),(5,4),(6,3), (7,2), (8,1);
