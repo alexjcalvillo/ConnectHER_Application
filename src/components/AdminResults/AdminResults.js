44d6bef816aec0a468da36cceb8e1520101ab3c1
Joe Thompson <smnindian30@yahoo.com>
2021-01-14 02:32:10 -0600

    Admin may now edit members, still needs styling

--- src/components/AdminResults/AdminResults.js
+++ src/components/AdminResults/AdminResults.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import { Col, Row } from 'reactstrap';
+import AdminMemberItem from '../AdminMemberItem/AdminMemberItem';
 
 // Displays results when placed in the Search Option map function
 class AdminResults extends Component {
   render() {
     return (
-      <div>
-        <tr>
-          <th></th>
-        </tr>
-        {this.props.results.map((item, i) => {
-          return <div></div>;
+      <Row>
+        {this.props.results.map((item, index) => {
+          return (
+            <Col className="col-spacing" key={index} lg={4} md={4} sm={12}>
+              <AdminMemberItem key={item.email} member={item} />
+            </Col>
+          );
         })}
-      </div>
+      </Row>
     );
   }
 }
